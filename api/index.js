6991eeee907d4f8dd42f2607e099394ab73ecfed
Wayd26 <wayedoun@gmail.com>
2023-02-03 13:03:48 +0100

    add neccessary CORS config to index.js

--- api/index.js
+++ api/index.js
@@ -20,6 +20,32 @@ mongoose.connect(process.env.MONGO_URI).then(
 
 app.use(cors())
 app.use(morgan('dev'));
+
+// CORS
+const whitelist = [
+    '*'
+  ];
+  
+  app.use((req, res, next) => {
+    const origin = req.get('referer');
+    const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+    if (isWhitelisted) {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+      res.setHeader('Access-Control-Allow-Credentials', true);
+    }
+    // Pass to next layer of middleware
+    if (req.method === 'OPTIONS') res.sendStatus(200);
+    else next();
+  });
+  
+  const setContext = (req, res, next) => {
+    if (!req.context) req.context = {};
+    next();
+  };
+  app.use(setContext);
+// CORS
 app.use(express.json())
 
 app.use('/api', authRoutes)
