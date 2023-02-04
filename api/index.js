7dc056289bb30e3e26dc915e11022b110ec09c7e
Wayd26 <wayedoun@gmail.com>
2023-02-04 07:44:32 +0100

    add neccessary CORS config to index.js

--- api/index.js
+++ api/index.js
@@ -34,24 +34,24 @@ app.use(cors({
 
   
   app.use((req, res, next) => {
-    const origin = req.get('referer');
-    const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-    if (isWhitelisted) {
+    // const origin = req.get('referer');
+    // const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+    // if (isWhitelisted) {
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
       res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
       res.setHeader('Access-Control-Allow-Credentials', true);
-    }
+    // }
     // Pass to next layer of middleware
     if (req.method === 'OPTIONS') res.sendStatus(200);
     else next();
   });
   
-  const setContext = (req, res, next) => {
-    if (!req.context) req.context = {};
-    next();
-  };
-  app.use(setContext);
+  // const setContext = (req, res, next) => {
+  //   if (!req.context) req.context = {};
+  //   next();
+  // };
+  // app.use(setContext);
 
 // CORS
 app.use(express.json())
