ccea7b4c99fb1c41754343a695f2a1afed74a9ab
Wayd26 <wayedoun@gmail.com>
2023-02-04 21:27:47 +0100

    update neccessary CORS config to index.js

--- api/index.js
+++ api/index.js
@@ -18,24 +18,13 @@ mongoose.connect(process.env.MONGO_URI).then(
     () => console.log("Database Connected Successfully")
 ).catch(error => console.log("Failed to connect to Database ", error))
 
-// app.use(cors({
-//   origin: '*', // use your actual domain name (or localhost), using * is not recommended
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-//   allowedHeaders: ['content-type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-//   credentials: true
-// }))
-
-app.use(cors())
 
 app.use(morgan('dev'));
 app.use(express.json())
 
 
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+app.use(cors());
+app.options('*', cors());
 
 app.use('/api', authRoutes)
 app.use('/api', categoryRoutes)
