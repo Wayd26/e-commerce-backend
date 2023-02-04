f365f5458e6b3c18d48ed040dfb9f0f24683f3fc
Wayd26 <wayedoun@gmail.com>
2023-02-04 21:36:32 +0100

    update neccessary CORS config to index.js

--- api/index.js
+++ api/index.js
@@ -22,9 +22,6 @@ mongoose.connect(process.env.MONGO_URI).then(
 app.use(morgan('dev'));
 app.use(express.json())
 
-
-app.use(cors());
-
 app.use(function(req, res, next) {
   var oneof = false;
   if(req.headers.origin) {
