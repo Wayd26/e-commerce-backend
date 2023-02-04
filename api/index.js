bbcdc39aaec8e5dd0a51ee737c004818595938e3
Wayd26 <wayedoun@gmail.com>
2023-02-04 08:25:36 +0100

    update neccessary CORS config to index.js

--- api/index.js
+++ api/index.js
@@ -40,7 +40,7 @@ app.use(cors({
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
       res.setHeader('Access-Control-Allow-Headers', '*');
-      res.setHeader('Access-Control-Allow-Credentials', true);
+      res.setHeader('Access-Control-Allow-Credentials', false);
     // }
     // Pass to next layer of middleware
     // if (req.method === 'OPTIONS') res.sendStatus(200);
