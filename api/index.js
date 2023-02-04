9dba23ff927a42acf18b5e2e4d5f7102eaca9e25
Wayd26 <wayedoun@gmail.com>
2023-02-04 18:56:13 +0100

    update neccessary CORS config to index.js

--- api/index.js
+++ api/index.js
@@ -30,19 +30,11 @@ app.use(express.json())
 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return next()
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+}) 
 
 app.use('/api', authRoutes)
 app.use('/api', categoryRoutes)
