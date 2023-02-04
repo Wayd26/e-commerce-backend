87c001111199af38b48ef66a5d85f4c5940b38fd
Wayd26 <wayedoun@gmail.com>
2023-02-04 19:03:16 +0100

    update neccessary CORS config to index.js

--- api/index.js
+++ api/index.js
@@ -30,9 +30,11 @@ app.use(express.json())
 
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+		res.setHeader("Access-Control-Max-Age", "1800");
+		res.setHeader("Access-Control-Allow-Headers", "content-type");
+		res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
   next();
 }) 
 
