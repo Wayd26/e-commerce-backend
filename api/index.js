888f72533f39ffd04f9a7e3eba7cdbc117a9f72b
Wayd26 <wayedoun@gmail.com>
2023-02-05 23:02:10 +0100

    update neccessary CORS config to index.js

--- api/index.js
+++ api/index.js
@@ -23,8 +23,17 @@ app.use(morgan('dev'));
 app.use(express.json())
 
 
+const allowedOrigins = ['https://open-shop.vercel.app', 'open-shop.vercel.app', 'https://www.open-shop.vercel.app']
 app.use(cors({
-  origin: false 
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+
 }));
 
 app.use('/api', authRoutes)
