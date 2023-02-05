1e1ab0a8b83077b133765f19dd5e3bf61c0b0ae4
Wayd26 <wayedoun@gmail.com>
2023-02-05 22:39:48 +0100

    update neccessary CORS config to index.js

--- api/index.js
+++ api/index.js
@@ -22,36 +22,12 @@ mongoose.connect(process.env.MONGO_URI).then(
 app.use(morgan('dev'));
 app.use(express.json())
 
-app.use(function(req, res, next) {
-  var oneof = false;
-  if(req.headers.origin) {
-      res.header('Access-Control-Allow-Origin', req.headers.origin);
-      oneof = true;
-  }
-  if(req.headers['access-control-request-method']) {
-      res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-      oneof = true;
-  }
-  if(req.headers['access-control-request-headers']) {
-      res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-      oneof = true;
-  }
-  if(oneof) {
-      res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-  }
 
-  // intercept OPTIONS method
-  if (oneof && req.method == 'OPTIONS') {
-      res.send(200);
-  }
-  else {
-      next();
-  }
-});
+// app.use(cors());
 
-app.use('/api', authRoutes)
-app.use('/api', categoryRoutes)
-app.use('/api', productRoutes)
+app.use('/api', cors(), authRoutes)
+app.use('/api', cors(), categoryRoutes)
+app.use('/api', cors(), productRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`)
