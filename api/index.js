2d4974085875abc3c6c4b7c10f1af663095ecbfc
Wayd26 <wayedoun@gmail.com>
2023-02-06 06:12:48 +0100

    update neccessary CORS config to api/index.js

--- api/index.js
+++ api/index.js
@@ -20,15 +20,11 @@ mongoose.connect(process.env.MONGO_URI).then(
 
 app.use(morgan('dev'));
 app.use(express.json())
-// app.use(cors())
+ app.use(cors())
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  return next()
-})
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+// })
 app.use('/api', authRoutes)
 app.use('/api', categoryRoutes)
 app.use('/api', productRoutes)
