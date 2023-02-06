1a5556ba605e235c442a67a0bf388e2f3272b5f2
Wayd26 <wayedoun@gmail.com>
2023-02-06 05:55:38 +0100

    update neccessary CORS config to api/index.js

--- api/index.js
+++ api/index.js
@@ -15,20 +15,26 @@ dotenv.config();
 const PORT = process.env.PORT || 8000;
 
 mongoose.connect(process.env.MONGO_URI).then(
-    () => console.log("Database Connected Successfully")
+  () => console.log("Database Connected Successfully")
 ).catch(error => console.log("Failed to connect to Database ", error))
 
 app.use(morgan('dev'));
 app.use(express.json())
 app.use(cors())
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  next()
+})
 app.use('/api', authRoutes)
 app.use('/api', categoryRoutes)
 app.use('/api', productRoutes)
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}...`)
+  console.log(`Server is running on port ${PORT}...`)
 
 })
 
- 
